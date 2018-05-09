0fcdba00a91807afb21dbf6e80f9926020bde6bb
vega2k <vega2k@naver.com>
2018-05-09 13:30:38 +0900

    form 사용, Post 등록

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     title=models.CharField(max_length=200)
     #글내용
     text=models.TextField()
+    #migration test
+    #test=models.TextField()
     #글작성일자
     created_date=models.DateTimeField(default=timezone.now)
     #글게시일자
